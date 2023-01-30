fb8748e1a709129202188d793512908caea45a49
Pete Thomae <Pete.Thomae@wwt.com>
2023-01-30 15:29:20 -0600

    Initial commit.

---
+++ Avocent_Port_Renamer.py
@@ -0,0 +1,88 @@
+from tkinter import *
+import os
+from netmiko import ConnectHandler
+import time
+
+main_prompt = '--:- / cli->'
+BLACK = "#100F0F"
+DARK_GRAY = "#0F3D3E"
+TAN = "#E2DCC8"
+WHITE = "#F1F1F1"
+
+def add():
+    device = {
+        'device_type': 'terminal_server',
+        'ip': ipv4_entry.get(),
+        'username': user_entry.get(),
+        'password': password_entry.get()
+    }
+
+    connection = ConnectHandler(**device)
+    time.sleep(3)
+
+    if '~]#' in connection.find_prompt():
+        connection.send_command('cli', read_timeout=0, expect_string=main_prompt)
+
+    port_names = []
+    with open('port_names.txt') as file:
+        for line in file:
+            port_names.append(line.strip())
+
+    if connection.find_prompt() == main_prompt:
+        connection.send_command("cd ports/serial_ports/", read_timeout=3, expect_string='--:- serial_ports cli->')
+        for x in range(1, len(port_names) + 1):
+            connection.send_command_timing(f'cd {x}/', read_timeout=0)
+            connection.send_command_timing('cd cas/', read_timeout=0)
+            connection.send_command_timing(f'set port_name={port_names[x - 1]}', read_timeout=0)
+            connection.send_command_timing('save', read_timeout=0)
+            print(f"Port {x} re-name complete.")
+
+def check_saved():
+    os.startfile(r'port_names.txt')
+
+tv = Tk()
+tv.minsize(width=400, height=400)
+tv.maxsize(width=400, height=400)
+tv.title("Avocent Port Namer")
+tv.config(bg="white")
+
+canvas = Canvas(width=400, height=200, highlightthickness=0, bg="white")
+logo = PhotoImage(file="avocent_cool_logo.png")
+canvas.create_image(180, 115, image=logo)
+canvas.place(x=20, y=-25)
+
+canvas2 = Canvas(width=500, height=50, highlightthickness=0, bg=DARK_GRAY)
+canvas2.place(x=0, y=0)
+
+canvas3 = Canvas(width=500, height=500, highlightthickness=0, bg=TAN)
+canvas3.place(x=0, y=180)
+
+site_text = Label(text="Avocent IP:", font=("Rockwell", 10), bg=TAN)
+site_text.place(x=25, y=230)
+
+user_text = Label(text="Username:", font=("Rockwell", 10), bg=TAN)
+user_text.place(x=25, y=260)
+
+password_text = Label(text="Password:", font=("Rockwell", 10), bg=TAN)
+password_text.place(x=25, y=290)
+
+add_button = Button(height=1, width=35, text="Rename Ports", font=("Rockwell", 10),
+                    relief="flat", bg=WHITE, command=add)
+add_button.place(x=127, y=325)
+
+saved_button = Button(height=1, width=12, text="Modify Names", font=("Rockwell", 10),
+                      relief="flat", bg=WHITE, command=check_saved)
+saved_button.place(x=25, y=325)
+
+ipv4_entry = Entry(width=42, bg=WHITE)
+ipv4_entry.place(x=127, y=232)
+
+user_entry = Entry(width=42, bg=WHITE)
+user_entry.place(x=127, y=263)
+
+password_entry = Entry(width=42, bg=WHITE)
+password_entry.place(x=127, y=293)
+
+data_box = Text(height=8, width=32)
+
+tv.mainloop()
